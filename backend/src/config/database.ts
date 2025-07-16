de2b51233e536ba2fb6f7d5c3d1335b3d6d52c6d
Bryan <bc683@cornell.edu>
2025-07-16 02:49:52 -0400

    idk what these unversioned files are but

---
+++ backend/src/config/database.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+// Create a global Prisma client instance
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // Enable logging
+});
+
+// Graceful shutdown
+process.on('beforeExit', async () => {
+  await prisma.$disconnect();
+});
+
+export { prisma };
